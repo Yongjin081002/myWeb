24a41c686032571f92a625c613513568266185af
Jin YongJin <163402661+Yongjin081002@users.noreply.github.com>
2024-11-25 19:20:55 +0900

    Delete nico_JS directory

--- nico_JS/color_selecter/random_food/script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector(".random_btn");
-const text = document.querySelector(".box div:first-child");
-const foodList = ['볶음밥','야채죽','치킨','햄버거']; 
-
-function Btn () {
-    const num = Math.floor(Math.random() * foodList.length);
-    console.log(num);
-    text.innerHTML = foodList[num];
-}
